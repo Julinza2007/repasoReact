f30bd88143bf92592a2a0bdc51ea86aa69750bb5
Julinza2007 <linzalatajulian@gmail.com>
2025-05-25 02:12:08 -0300

    Agrego workflows para desplegar cada SPA

--- proyectos/introduccion/vite.config.js
+++ proyectos/introduccion/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+const repo = process.env.REPO_NAME || 'repasoReact';
+const project = process.env.PROJECT_DIR || 'introduccion';
+
 export default defineConfig({
   plugins: [react()],
-})
